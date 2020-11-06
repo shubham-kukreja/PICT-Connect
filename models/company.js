0d55f203bcb084924f4fe2e090f45179600dee63
Maitreyi Sawant <50900909+maitreyi00@users.noreply.github.com>
2020-11-06 18:42:05 +0530

    Update company.js

---
+++ models/company.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema({
+  name: String,
+  city: String,
+  cutoff: Number,
+  criteria: String,
+  total_selected: Number,
+  visit_date: Date,
+  ctc: Number
+});
+
+const Company = mongoose.model("Companies", companySchema);
+
+module.exports = {
+  Player: Player,
+};
